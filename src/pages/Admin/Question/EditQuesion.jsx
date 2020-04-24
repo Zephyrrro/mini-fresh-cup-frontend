f586e47b7d55d77f3d729f8692f43b2ec01c23cb
Sunboy482 <1157014557@qq.com>
2020-04-24 18:02:14 +0800

    xxxx

--- src/pages/Admin/Question/EditQuesion.jsx
+++ src/pages/Admin/Question/EditQuesion.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { Table } from 'antd';
+const { Column } = Table;
+
+function GetData(){
+  return [
+    {
+      title:'1',
+      content:'2'
+    }
+  ];
+}
 
 export default function EditQuestion() {
-  return <div className="login">EditQuestion Page</div>;
+  return   <Table dataSource={GetData}>
+    <Column title="题目描述" dataIndex="title" key="title" />
+    <Column title="答案" dataIndex="content" key="content" />
+    <Column
+      title="Action"
+      key="action"
+      render={(text, record) => (
+        <span>
+          <a style={{ marginRight: 16 }}>上升</a>
+          <a style={{ marginRight: 16 }}>下降</a>
+          <a>删除</a>
+        </span>
+      )}
+    />
+  </Table>;
 }

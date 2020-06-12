1984bffe9dfd6f2c5efe847cf2b96efaf2bd0dd6
Sunboy482 <1157014557@qq.com>
2020-06-12 13:40:19 +0800

    界面美化，新增状态提示

--- src/pages/Answer/cookie.jsx
+++ src/pages/Answer/cookie.jsx
@@ -13,3 +13,8 @@ export function getCookie(cname) {
   }
   return '';
 }
+
+export function deleteCookie(cname) {
+  document.cookie= `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+  console.log(document.cookie);
+}
--- src/pages/Answer/index.jsx
+++ src/pages/Answer/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Layout, List, Space, Button, Input, Divider,Tag,Row,Col,message } from 'antd';
-import { QuestionCircleOutlined,MessageOutlined,CheckOutlined } from '@ant-design/icons';
-import { getQuestion, getQuestionAnswer, answerQuestion,getNotice } from '@/api';
-import {getCookie} from '@/pages/Answer/cookie';
+import { Layout, List, Space, Button, Input, Divider, Tag, Row, Col, message, Result } from 'antd';
+import { QuestionCircleOutlined, MessageOutlined, SmileOutlined } from '@ant-design/icons';
+import { getQuestion, getQuestionAnswer, answerQuestion, getNotice } from '@/api';
+import { deleteCookie, getCookie } from '@/pages/Answer/cookie';
+import './index.less';
 
 const { Content, Sider } = Layout;
 
-class NoticeForm extends React.Component{
-  noticeTimer(){
-    getNotice().then(res=>{
-      this.setState({NoticeArray:res.data.data.notices.reverse()});
-    })
+class NoticeForm extends React.Component {
+  noticeTimer() {
+    getNotice().then(res => {
+      this.setState({ NoticeArray: res.data.data.notices.reverse() });
+    });
   }
+
   componentDidMount() {
     this.noticeTimer();
     this.timerID = setInterval(
@@ -19,18 +21,22 @@ class NoticeForm extends React.Component{
       2000
     );
   }
-  componentWillUnmount(){
+
+  componentWillUnmount() {
     clearInterval(this.timerID);
   }
+
   constructor(props) {
     super(props);
-    this.state={NoticeArray:[]}
+    this.state = { NoticeArray: [] };
   }
 
   render() {
     return (
       <div>
+        <Divider orientation="left">公告区</Divider>
         <List
+          className="notice-form"
           bordered
           itemLayout="horizontal"
           /* eslint-disable-next-line react/destructuring-assignment */
@@ -40,7 +46,7 @@ class NoticeForm extends React.Component{
               <Space>
                 <MessageOutlined style={{ fontSize: '20px' }}/>
                 <List.Item.Meta
-                  title={item.id+":"+item.title}
+                  title={item.id + ':' + item.title}
                   description={
                     item.content
                   }
@@ -66,7 +72,7 @@ class QuestionList extends React.Component {
   render() {
     const { QuestionArray } = this.props;
     return (
-      <div className="Question">
+      <div className="question-list">
         <List
           bordered
           itemLayout="horizontal"
@@ -94,49 +100,108 @@ class QuestionList extends React.Component {
 }
 
 class AnswerForm extends React.Component {
+  Tag() {
+    const {StatusCode}=this.props;
+    var Color,Message;
+    switch (StatusCode) {
+      case -1:
+        Color="default";
+        Message="未答";
+        break;
+      case 0:
+        Color="success";
+        Message="已提交";
+        break;
+      case 1:
+        Color="warning";
+        Message="未提交";
+        break;
+    }
+    return (<Tag className="answer-tag" color={Color}>
+      {Message}
+    </Tag>);
+  }
+
   constructor(props) {
     super(props);
   }
 
   render() {
+    if (this.props.id === -1) {
+      return (
+        <Result
+          icon={<SmileOutlined />}
+          title="现在开始愉快地答题吧!"
+          subTitle="温馨提示：请关注右侧的公告栏以便及时获取信息"
+          extra={[]}
+        />
+      );
+    }
     return (
-      <div className="AnswerForm">
+      <div className="answer-form">
         <Divider orientation="left">题目{+this.props.id}</Divider>
         <p>{this.props.question}</p>
-        <Input.TextArea value={this.props.content} onChange={e => this.props.handleChange(e.target.value)}/>
-        <Button icon={<CheckOutlined />} onClick={e =>this.props.handleSave()}>提交</Button>
+        <Input.TextArea
+          className="answer-input"
+          value={this.props.content} onChange={e => this.props.handleChange(e.target.value)}/>
+        <Button onClick={e => this.props.handleSave()}>提交</Button>
+        {this.Tag()}
       </div>
     );
   }
 }
 
 class AnswerPage extends React.Component {
-  handleSave(){
-    answerQuestion({ questionId: this.state.AnswerId, content: this.state.Answer }).then(res=>{
-      if(res.data.success===true){
-        message.success("提交成功！")
-      }else
-        message.error("出现错误！错误码"+res.data.statusCode);
-    })
+  handleSave() {
+    answerQuestion({ questionId: this.state.AnswerId, content: this.state.Answer }).then(res => {
+      if (res.data.success === true) {
+        message.success('提交成功！');
+        this.setState({ StatusCode: 0 });
+        deleteCookie('id' + this.state.AnswerId);
+        console.log(document.cookie);
+      } else
+        message.error('出现错误！错误码' + res.data.statusCode);
+    });
   }
+
   handleChange = (value) => {
-    this.setState({Answer:value});
-    document.cookie="id"+this.state.AnswerId+"="+encodeURIComponent(value)+";SameSite=Strict";
+    this.setState({ Answer: value });
+    this.setState({ StatusCode: 1 });
+    document.cookie = 'id' + this.state.AnswerId + '=' + encodeURIComponent(value) + ';SameSite=Strict';
     console.log(document.cookie);
   };
 
   GetAnswer(id) {
     const cookie = getCookie('id' + id);
-    if(cookie!='')
-      this.setState({ Answer: cookie });
-    else
+    var answer = '';
     getQuestionAnswer().then(res => {
+      //console.log(res.data.data.answer)
       res.data.data.answer.map(obj => {
         if (obj.question.id === id) {
-          this.setState({ Answer: obj.content });
+          answer = obj.content;
         }
       });
+      message.info(`cookie:${cookie} answer:${answer}`);
+      if (cookie === '' && answer === '') {
+        message.info('还没开始做');
+        this.setState({ Answer: answer, StatusCode: -1 });
+        return;
+      }
+      if (cookie === '' && answer != '') {
+        message.info('服务器有答案');
+        this.setState({ Answer: answer, StatusCode: 0 });
+        return;
+      }
+      if (cookie === answer) {
+        message.success('cookie和服务器答案一致');
+        this.setState({ Answer: answer, StatusCode: 0 });
+        return;
+      } else {
+        message.info('cookie和服务器答案不一致');
+        this.setState({ Answer: cookie, StatusCode: 1 });
+      }
     });
+
   }
 
   SetAnswerId(id) {
@@ -166,13 +231,13 @@ class AnswerPage extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { QuestionArray: [], AnswerId: -1, Question: '', Answer: '' };
+    this.state = { QuestionArray: [], AnswerId: -1, Question: '', Answer: '', StatusCode: -1 };
   }
 
   render() {
     return (
       <Layout>
-        <Sider style={{ margin: '0 20px 0' }}>
+        <Sider className="answer-sider">
           <QuestionList QuestionArray={this.state.QuestionArray} SetId={this.SetAnswerId.bind(this)}/>
         </Sider>
         <Layout style={{ padding: '0 24px 24px' }}>
@@ -180,13 +245,14 @@ class AnswerPage extends React.Component {
             <Row>
               <Col span={15}>
                 <AnswerForm id={this.state.AnswerId} question={this.state.Question} content={this.state.Answer}
+                            StatusCode={this.state.StatusCode}
                             handleChange={this.handleChange.bind(this)}
                             handleSave={this.handleSave.bind(this)}
                 />
               </Col>
-              <Col span={1} />
+              <Col span={1}/>
               <Col span={8}>
-                <NoticeForm />
+                <NoticeForm/>
               </Col>
             </Row>
           </Content>
@@ -198,7 +264,7 @@ class AnswerPage extends React.Component {
 
 export default function Answer() {
   return (
-    <div className="answer">
+    <div>
       <AnswerPage/>
     </div>
   );
